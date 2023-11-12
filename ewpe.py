ac78f386513d55b5c9ddd204b46e89b3ac6af4e7
refa3211 <111779078+refa3211@users.noreply.github.com>
2023-11-12 11:51:11 +0200

    update

--- ewpe.py
+++ ewpe.py
@@ -7,12 +7,11 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import json
 import socket
-from writejson import *
 
 GENERIC_KEY = "a3K8Bx%2r8Y7#xDh"
 
-paramlist: list[str] = ["Pow", "Mod", "SetTem", "WdSpd", "Air", "Blo", "Health", "SwhSlp", "Lig", "SwingLfRig",
-                        "SwUpDn", "Quiet", "Tur", "StHt", "TemUn", "HeatCoolType", "TemRec", "SvSt"]
+paramlist: list[str] = ['Pow', 'Blo', 'Mod', 'mac', 'Air', 'Health', 'SwUpDn', 'Quiet', 'Tur', 'TemUn', 'TemRec',
+                        'SvSt', 'HeatCoolType', 'StHt', 'SetTem', 'WdSpd']
 
 
 class ScanResult:
@@ -58,9 +57,7 @@ def create_cipher(key):
 def decrypt(pack_encoded, key):
     decryptor = create_cipher(key).decryptor()
     pack_decoded = base64.b64decode(pack_encoded)
-    # print(f'\npack_decoded: {pack_decoded}')
     pack_decrypted = decryptor.update(pack_decoded) + decryptor.finalize()
-    # print(f'pack_decrypted: {pack_decrypted}')
     pack_unpadded = pack_decrypted[0:pack_decrypted.rfind(b'}') + 1]
     return pack_unpadded.decode('utf-8')
 
@@ -83,77 +80,66 @@ def encrypt_generic(pack):
 
 
 def search_devices(broadcast=None):
-    if len(datalist) == 0:
-        if broadcast is None:
-            broadcast = '192.168.0.255'
-        print(f'Searching for devices using broadcast address: {broadcast}')
+    if broadcast is None:
+        broadcast = '192.168.0.255'
+    print(f'Searching for devices using broadcast address: {broadcast}')
 
-        s = socket.socket(type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
-        s.settimeout(5)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        s.sendto(b'{"t":"scan"}', (broadcast, 7000))
+    s = socket.socket(type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
+    s.settimeout(5)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    s.sendto(b'{"t":"scan"}', (broadcast, 7000))
 
-        results = []
+    results = []
 
-        while True:
-            try:
-                (data, address) = s.recvfrom(1024)
-                if len(data) == 0:
-                    continue
+    while True:
+        try:
+            (data, address) = s.recvfrom(1024)
 
-                raw_json = data[0:data.rfind(b"}") + 1]
+            if len(data) == 0:
+                continue
 
-                resp = json.loads(raw_json)
-                # print(f'resp: {resp}')
-                pack = json.loads(decrypt_generic(resp['pack']))
-                # print(f'pack: {pack}')
+            raw_json = data[0:data.rfind(b"}") + 1]
+
+            resp = json.loads(raw_json)
+            pack = json.loads(decrypt_generic(resp['pack']))
 
 
-                cid = pack['cid'] if 'cid' in pack and len(pack['cid']) > 0 else \
-                    resp['cid'] if 'cid' in resp else '<unknown-cid>'
+            cid = pack['cid'] if 'cid' in pack and len(pack['cid']) > 0 else \
+                resp['cid'] if 'cid' in resp else '<unknown-cid>'
 
-                results.append(ScanResult(address[0], address[1], cid, pack['name'] if 'name' in pack else '<unknown>'))
+            results.append(ScanResult(address[0], address[1], cid, pack['name'] if 'name' in pack else '<unknown>'))
 
-            except socket.timeout:
-                print(f'Search finished, found {len(results)} device(s)')
-                break
+        except socket.timeout:
+            print(f'Search finished, found {len(results)} device(s)')
+            break
 
-        if len(results) > 0:
+    if len(results) > 0:
+        for r in results:
+            bind_device(r)
 
-            for r in results:
-                bind_device(r)
-
-            writetofile()
-
-        return results
-    else:
-        return datalist
+    return results
 
 
 def bind_device(search_result) -> tuple[Any, Any, Any, Any]:
+
     print(f'Binding device: {search_result.ip} ({search_result.name}, ID: {search_result.id})\n')
 
     pack = '{"mac":"%s","t":"bind","uid":0}' % search_result.id
     pack_encrypted = encrypt_generic(pack)
 
-    request = create_request(search_result.id, pack_encrypted, i=1)
+    request = create_request(search_result.id, pack_encrypted, 1)
     result = send_data(search_result.ip, 7000, bytes(request, encoding='utf-8'))
 
     response = json.loads(result)
-
     if response["t"] == "pack":
         pack = response["pack"]
         pack_decrypted = decrypt_generic(pack)
 
         bind_resp = json.loads(pack_decrypted)
-        # print(f'bind_resp: {bind_resp}')
 
         if 't' in bind_resp and bind_resp["t"].lower() == "bindok":
             key = bind_resp['key']
-
-            write_jsontolist(ip=search_result.ip, mac=search_result.name, key=key)
-
             print('Bind to %s succeeded, key = %s' % (search_result.id, key))
             return key
 
@@ -167,7 +153,6 @@ def get_param(id, key, ipclient, params=None):
 
     pack = f'{{"cols":[{cols}],"mac":"{id}","t":"status"}}'
     pack_encrypted = encrypt(pack, key)
-    print(f'pack encrypted\n {pack_encrypted} \n{key}')
 
     request = '{"cid":"app","i":0,"pack":"%s","t":"pack","tcid":"%s","uid":0}' % (pack_encrypted, id)
     result = send_data(ipclient, 7000, bytes(request, encoding='utf-8'))
@@ -178,7 +163,6 @@ def get_param(id, key, ipclient, params=None):
 
         pack_decrypted = decrypt(pack, key)
         pack_json = json.loads(pack_decrypted)
-        print(f'pack_json: {pack_json}')
 
         for key, value in zip(pack_json['cols'], pack_json['dat']):
             print(f'{key} : {value}')
@@ -210,6 +194,9 @@ def set_param(id, key, ipclient, params=""):
 
         pack_decrypted = decrypt(pack, key)
         pack_json = json.loads(pack_decrypted)
+
+        # dump data to file
+        # pack_json.json()
 
         if pack_json['r'] == 200:
             print('Parameter set successfully')
